bb4c7e36526004585ab5613e64362087a046c6ce
Noah Meisel <nmeisel@ucsc.edu>
2025-03-15 14:17:29 -0700

    SEAL fully functional. Only issue is the restriction from ORAM.rs that data needs to be arrays of size N, so had to pad u64 entries with 0

--- project/src/SEAL.rs
+++ project/src/SEAL.rs
@@ -6,32 +6,44 @@ use aes_gcm::{
 use rand::rngs::OsRng;
 use crate::ORAM::{Z, L, N};
 use aes_gcm::AeadInPlace;
+use std::fmt;
+use rand::Rng;
+use permutation_iterator::Permutor;
+use std::collections::HashSet;
+
+
 
 pub struct SEAL {
-    pub k: Key<Aes256Gcm>,
+    pub k: u64,
     pub alpha: u64,
+    pub maxV: u64,
 }
 
 impl SEAL {
     pub fn new(alpha: u64) -> Self {
+        let mut rng = rand::thread_rng();
         SEAL {
-            k: Aes256Gcm::generate_key(OsRng),
+            k: rng.gen(),
             alpha,
+            maxV: 0,
         }
     }
 
-    pub fn ADJOramInit(&mut self, securityParam: u64,array: Vec<u64>,alpha: u64)-> Vec<ORAM> {
+    pub fn ADJOramInit(&mut self, array: Vec<u64>,alpha: u64)-> Vec<ORAM> {
         self.alpha = alpha;
+        self.maxV = array.len() as u64;
         let mew = 2_i32.pow(self.alpha as u32) as usize;
         // Intitalizing the arrays based on alpha
-        let s_size = mew.clone() / array.len();
+        println!("Array Len: {}", array.len());
+        let s_size =  array.len() / mew.clone();
+        println!("S Dimensions: {} {}", mew, s_size);
         let mut S = vec![vec![0u8; s_size]; mew];
-        let cipher = Aes256Gcm::new(&self.k);
         // 5-7, not sure if there is a better way of doing this.
         //Basically using a block cipher as a PRP which should work?
         for i in 0..array.len() {
-            let (l, phi) = Self::compute_phi_and_l(i, self.alpha, &cipher);
-            S[l+1][phi+1] = array[i] as u8;
+            let (l, phi) = self.compute_phi_and_l(i, self.alpha);
+            println!("L and Phi: {} {}",l,phi);
+            S[l][phi] = array[i] as u8;
         }
 
         // Loop intalizes S. We now want to store each array in S in it's own ORAM
@@ -40,38 +52,53 @@ impl SEAL {
         oram_array
     }
     // &mut self, op: String, address: u64, data_new: [u8; N]) -> [u8; N]
-    pub fn ADJOramAccess(&mut self, op: String, i: u64, vSubi: [u8; N], mut EM: Vec<ORAM>) -> [u8; N] {
+    pub fn ADJOramAccess(&mut self, op: String, i: u64, vSubi: [u8; N],  EM: &mut Vec<ORAM>) -> Result<[u8; N], SEALError> {
+        if let maxV = self.maxV {
+            if i > maxV {
+                return Err(SEALError::IndexOutOfBounds);
+            }
+        } else {
+            return Err(SEALError::IndexOutOfBounds);
+        }
+
         let mew = 2_i32.pow(self.alpha as u32) as usize;
-        let cipher = Aes256Gcm::new(&self.k);
-        let (l, phi) = Self::compute_phi_and_l(i as usize, self.alpha, &cipher);
-        let result = EM[l+1].access(op,(phi+1).try_into().unwrap(),vSubi);
-        result 
+        let (l, phi) = self.compute_phi_and_l(i as usize, self.alpha);
+        let result = EM[l].access(op,(phi).try_into().unwrap(),vSubi);
+        Ok(result)
     }
     fn initialize_oram_array(&mut self,mew: usize,S: Vec<Vec<u8>>) -> Vec<ORAM> {
         let mut oram_array = Vec::with_capacity(mew);
-        let cipher = Aes256Gcm::new(&self.k);
         for oram_num in 0..mew {
             let mut oram = ORAM::new();
             oram.init();
             // TODO: Not sure about the indexing here. Might need assistance troubleshooting this.
             for i in 0..(S[oram_num].len()) {
-                let (l, phi) = Self::compute_phi_and_l(i, self.alpha, &cipher);
-                let padded_data = Self::pad_to_length(vec![S[oram_num][l+1]], N);
-                oram.access("write".to_string(), (phi+1).try_into().unwrap(), padded_data);
+                println!("I: {}", i);
+                let (l, phi) = self.compute_phi_and_l(i, self.alpha);
+                println!("L and Phi: {} {}",l,phi);
+                let padded_data = Self::pad_to_length(vec![S[oram_num][i]], N);
+                oram.access("write".to_string(), (i).try_into().unwrap(), padded_data);
             }
             // TODO: Not sure if this is the correct way we should be initalizing each ORAM. Might need another helper for multiple access.
             oram_array.push(oram);
         }
         oram_array
     }
-    fn compute_phi_and_l(i: usize, alpha: u64, cipher: &Aes256Gcm) -> (usize, usize) {
-        let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-        let mut block = i.to_le_bytes().to_vec();
-        cipher.encrypt_in_place(&nonce, b"", &mut block).expect("encryption failure!");
-        let array: [u8; 8] = block.try_into().expect("Not 8 element vec");
-        let prp_output = u64::from_le_bytes(array);
-        let l = (prp_output >> (64 - alpha)) as usize;
-        let phi = (prp_output & ((1 << (64 - alpha)) - 1)) as usize;
+    fn compute_phi_and_l(&self, i: usize, alpha: u64) -> (usize, usize) {
+        // let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+        // println!("{}", i);
+        // let mut block = (i as u64).to_le_bytes().to_vec();
+        // println!("{:?}", block);
+        // cipher.encrypt_in_place(&nonce, b"", &mut block).expect("encryption failure!");
+        // let array: [u8; 8] = truncate_to_8_elements(block);
+        let mut p = Permutor::new_with_u64_key(self.maxV, self.k);
+        let prp_output = p.nth(i).expect("Failed to Permute") & 0b111;
+        println!("PRP Output: {:?}", prp_output);
+
+        // let num_bits = 64 - prp_output.leading_zeros() as u64;
+
+        let l = (prp_output >> (3 - alpha)) as usize;
+        let phi = (prp_output & ((1 << (3 - alpha)) - 1)) as usize;
         (l, phi)
     }
     fn pad_to_length(mut data: Vec<u8>, length: usize) -> [u8; N] {
@@ -81,3 +108,25 @@ impl SEAL {
         array
     }
 }
+
+fn truncate_to_8_elements(data: Vec<u8>) -> [u8; 8] {
+    let mut array = [0u8; 8];
+    let slice = &data[..8]; 
+    array.copy_from_slice(slice);
+    array
+}
+
+#[derive(Debug)]
+pub enum SEALError {
+    IndexOutOfBounds,
+}
+
+impl fmt::Display for SEALError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SEALError::IndexOutOfBounds => write!(f, "Index out of bounds"),
+        }
+    }
+}
+
+impl std::error::Error for SEALError {}
